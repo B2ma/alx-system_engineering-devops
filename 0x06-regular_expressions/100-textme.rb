8c74db4ec3b7df5b0aaca9b7d24509dd301efdf8
B2ma <beatrice.terer@gmail.com>
2023-09-08 09:02:43 +0000

    Corrected 100-textme.rb

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,2 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/from+:(\w+|\+?\d+)|to+:(\+?+\d+)|flags+:(-+.{11,12})/).map(&:compact).join(',')
+puts ARGV[0].scan(/from+:(\w+|\+?\d+)|to+:(\+?+\d+)|flags+:(-+.{11,13})/).map(&:compact).join(',')
