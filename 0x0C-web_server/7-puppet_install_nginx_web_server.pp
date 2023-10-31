1f74a26748332feecb4ee8042aeca67b5ab6a820
B2ma <beatrice.terer@gmail.com>
2023-10-31 15:07:42 +0000

    Updated 7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -6,23 +6,44 @@
 # The redirection must be a “301 Moved Permanently”
 # This file is a Puppet manifest containing commands to automatically configure an Ubuntu machine to respect above requirements
 
-package { 'nginx':
-  ensure => 'installed'
+# Check if Nginx is installed, if not, install it
+if ! Package['nginx'] {
+  package { 'nginx':
+    ensure => 'installed',
+  }
 }
 
-file { '/var/www/html/index.html':
-  content => 'Hello World',
+# Define a class for Nginx configuration
+class { 'nginx':
+  package_manage => false, # Prevent Puppet from managing the package again
+  service_manage => true,
+  service_enable => true,
 }
 
-file_line { 'redirection-301':
-  ensure => 'present',
-  path   => '/etc/nginx/sites-available/default',
-  after  => 'listen 80 default_server;',
-  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+nginx::resource::vhost { 'default':
+  listen_port => '80',
+  proxy      => 'false',
+  rewrite    => [
+    '^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent',
+  ],
+  error_page => {
+    '404' => '/404.html',
+  },
+  location   => {
+    '/' => {
+      try_files => '$uri $uri/ =404',
+    },
+  },
+}
+
+file { '/usr/share/nginx/html/404.html':
+  ensure  => file,
+  content => "Ceci n'est pas une page\n",
 }
 
 service { 'nginx':
-  ensure  => running,
+  ensure  => 'running',
   enable  => true,
-  require => File['/etc/nginx/sites-available/default'],
+  require => Nginx::Resource::Vhost['default'],
 }
+
