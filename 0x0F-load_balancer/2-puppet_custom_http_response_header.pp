5f26aeb1c74973da7b437b22b0f08b6f96641f8f
B2ma <beatrice.terer@gmail.com>
2023-11-11 07:38:28 +0000

    updated 2-puppet_custom_http_response_header.pp

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -2,31 +2,10 @@
 
 # The name of the custom HTTP header must be X-Served-By
 # The value of the custom HTTP header must be the hostname of the server Nginx is running on
-exec { 'configure_nginx':
-  command => '/bin/bash -c "
-    apt-get update && apt-get install -y nginx &&
-    echo \"Hello world!\" > /var/www/html/index.html &&
-    echo \"Ceci n'est pas une page\" > /var/www/html/404.html &&
-    cat > /etc/nginx/sites-available/default << EOF
-      server {
-          listen 80 default_server;
-          listen [::]:80 default_server;
-          add_header X-Served-By $HOSTNAME;
-          root   /var/www/html;
-          index  index.html index.htm;
-
-          location /redirect_me {
-              return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
-          }
-
-          error_page 404 /404.html;
-
-          location /404 {
-              root /var/www/html;
-              internal;
-          }
-      }
-EOF
-    service nginx restart
-  "',
+exec { 'command':
+  command  => 'apt-get -y update;
+  apt-get -y install nginx;
+  sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
+  service nginx restart',
+  provider => shell,
 }
