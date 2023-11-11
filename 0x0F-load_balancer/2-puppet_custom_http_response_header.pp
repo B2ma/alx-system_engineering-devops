d44549ac1fb16f07bd616cec6d657a93b838f65a
B2ma <beatrice.terer@gmail.com>
2023-11-11 07:35:45 +0000

    updated 2-puppet_custom_http_response_header.pp

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -2,49 +2,31 @@
 
 # The name of the custom HTTP header must be X-Served-By
 # The value of the custom HTTP header must be the hostname of the server Nginx is running on
-package { 'nginx':
-  ensure => installed,
-}
+exec { 'configure_nginx':
+  command => '/bin/bash -c "
+    apt-get update && apt-get install -y nginx &&
+    echo \"Hello world!\" > /var/www/html/index.html &&
+    echo \"Ceci n'est pas une page\" > /var/www/html/404.html &&
+    cat > /etc/nginx/sites-available/default << EOF
+      server {
+          listen 80 default_server;
+          listen [::]:80 default_server;
+          add_header X-Served-By $HOSTNAME;
+          root   /var/www/html;
+          index  index.html index.htm;
 
-file { '/var/www/html/index.html':
-  content => 'Hello world!',
-}
+          location /redirect_me {
+              return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+          }
 
-file { '/var/www/html/404.html':
-  content => "Ceci n'est pas une page",
-}
+          error_page 404 /404.html;
 
-exec { 'set_nginx_header':
-  command => '/bin/bash -c "echo \"X-Served-By $HOSTNAME;\" > /etc/nginx/sites-available/default"',
-  notify  => Service['nginx'],
-  require => Package['nginx'],
-}
-
-file { '/etc/nginx/sites-available/default':
-  content => @(EOF),
-    server {
-        listen 80 default_server;
-        listen [::]:80 default_server;
-        add_header X-Served-By $HOSTNAME;
-        root   /var/www/html;
-        index  index.html index.htm;
-
-        location /redirect_me {
-            return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
-        }
-
-        error_page 404 /404.html;
-
-        location /404 {
-            root /var/www/html;
-            internal;
-        }
-    }
-    EOF
-  notify  => Service['nginx'],
-}
-
-service { 'nginx':
-  ensure => running,
-  enable => true,
+          location /404 {
+              root /var/www/html;
+              internal;
+          }
+      }
+EOF
+    service nginx restart
+  "',
 }
