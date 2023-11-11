211dffccd3e29f4873709431694ac6312e6d17e4
B2ma <beatrice.terer@gmail.com>
2023-11-11 07:25:19 +0000

    updated 2-puppet_custom_http_response_header.pp

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,7 +1,7 @@
-- This automate the task of creating a custom HTTP header response, but with Puppet.
+# This automate the task of creating a custom HTTP header response, but with Puppet.
 
-- The name of the custom HTTP header must be X-Served-By
-- The value of the custom HTTP header must be the hostname of the server Nginx is running on
+# The name of the custom HTTP header must be X-Served-By
+# The value of the custom HTTP header must be the hostname of the server Nginx is running on
 package { 'nginx':
   ensure => installed,
 }
