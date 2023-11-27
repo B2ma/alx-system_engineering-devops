ed0a1b30c44067bfe4a0d448073efe0218b48501
B2ma <beatrice.terer@gmail.com>
2023-11-27 10:00:20 +0000

    Creates task 1 and updates task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -2,60 +2,34 @@
 """
 Fetches and displays the TODO list progress for a given employee ID.
 """
-
 import requests
 import sys
 
 
-def get_employee_todo_progress(employee_id):
-    """
-    Fetches and displays the TODO list progress for a given employee ID.
-
-    Args:
-        employee_id (int): The ID of the employee.
-
-    Returns:
-        None
-    """
-    # Fetching user data
-    url_user = f'https://jsonplaceholder.typicode.com/users/{employee_id}'
-    response_user = requests.get(url_user)
-    user_data = response_user.json()
-
-    if 'name' not in user_data:
-        print(f"Employee with ID {employee_id} not found.")
-        return
-
-    # Extracting first and last names from the 'name' field
-    f_name, l_name = user_data['name'].split(' ', 1)\
-        if ' ' in user_data['name'] else (user_data['name'], '')
-
-    # API endpoint URL for TODOs
-    url_tdo = f'https://jsonplaceholder.typicode.com/users/{employee_id}/todos'
-
-    # Fetching TODO list data
-    response_todos = requests.get(url_tdo)
-    todos = response_todos.json()
-
-    if not todos:
-        print(f"Employee with ID {employee_id} not found.")
-        return
-
-    # Extracting relevant information
-    ttl_t = len(todos)
-    dn_t = [task for task in todos if task.get('completed', False)]
-
-    # Displaying the progress
-    print(
-        f"Employee {f_name} {l_name} is done with tasks({len(dn_t)}/{ttl_t}):")
-    for task in dn_t:
-        print(f"\t{task.get('title', '')}")
-
-
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python script.py <employee_id>")
-        sys.exit(1)
+    responseTodos = requests.get('https://jsonplaceholder.typicode.com/todos/')
+    data_todos = responseTodos.json()
+    completed_tasks = 0
+    total_tasks = 0
+    task_list = []
+    response_users = requests.get('https://jsonplaceholder.typicode.com/users')
+    data_users = response_users.json()
 
-    employee_id = int(sys.argv[1])
-    get_employee_todo_progress(employee_id)
+    for user_entry in data_users:
+        if user_entry.get('id') == int(sys.argv[1]):
+            employee_name = user_entry.get('name')
+
+    for todo_entry in data_todos:
+        if todo_entry.get('userId') == int(sys.argv[1]):
+            total_tasks += 1
+
+            if todo_entry.get('completed') is True:
+                completed_tasks += 1
+                task_list.append(todo_entry.get('title'))
+
+    print("Employee {} is done with tasks({}/{}):".format(employee_name,
+                                                          completed_tasks,
+                                                          total_tasks))
+
+    for task_item in task_list:
+        print("\t {}".format(task_item))
---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Fetches and displays the TODO list progress for a given employee ID.
+"""
+
+import requests
+import sys
+import csv
+
+
+if __name__ == "__main__":
+    todosResponse = requests.get('https://jsonplaceholder.typicode.com/todos/')
+    todos_data = todosResponse.json()
+
+    user_response = requests.get('https://jsonplaceholder.typicode.com/users')
+    users_data = user_response.json()
+
+    for user in users_data:
+        if user['id'] == int(sys.argv[1]):
+            employee_username = user['username']
+
+    with open(sys.argv[1] + '.csv', 'w', newline='') as csv_file:
+        csv_writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
+
+        for todo in todos_data:
+            if todo['userId'] == int(sys.argv[1]):
+                row = [
+                    todo['userId'],
+                    employee_username,
+                    todo['completed'],
+                    todo['title']
+                ]
+                csv_writer.writerow(row)
