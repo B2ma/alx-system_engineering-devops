7b9793dff7f71cf4874c90d43f09b083e99a9c49
B2ma <beatrice.terer@gmail.com>
2023-11-27 11:18:37 +0000

    Completes task 3

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+Python script to export data in the JSON format
+Records all tasks from all employees
+"""
+import json
+import requests
+
+
+if __name__ == "__main__":
+    tasks_response = requests.get('https://jsonplaceholder.typicode.com/todos/')
+    tasks_data = tasks_response.json()
+
+    user_response = requests.get('https://jsonplaceholder.typicode.com/users')
+    users_data = user_response.json()
+
+    user_task_dict = {}
+
+    for user in users_data:
+        user_tasks = []
+
+        for task in tasks_data:
+            task_dict = {}
+
+            if user['id'] == task['userId']:
+                task_dict['username'] = user['username']
+                task_dict['task'] = task['title']
+                task_dict['completed'] = task['completed']
+                user_tasks.append(task_dict)
+
+        user_task_dict[user['id']] = user_tasks
+
+    with open("todo_all_employees.json", "w") as file:
+        json_object = json.dumps(user_task_dict)
+        file.write(json_object)
